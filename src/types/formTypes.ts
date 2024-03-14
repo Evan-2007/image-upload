13ecf83ea15a00bead776d5e78b999b717625751
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2024-03-14 16:21:56 -0700

    fixed type errors

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -5,21 +5,21 @@ const MAX_FILE_SIZE = 25000000 ;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
 export const Schema = z.object({
-    file: z
-      .instanceof(File) // Start with a more specific type
-      .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
-        message: "Only .jpg, .jpeg, .png, and .webp formats are supported.",
-      })
-      .refine((file) => file.size <= MAX_FILE_SIZE, {
-        message: "Max image size is 25MB.",
-      })
-  });
+  file: z
+    .any() // Start with a more specific type
+    .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
+      message: "Only .jpg, .jpeg, .png, and .webp formats are supported.",
+    })
+    .refine((file) => file.size <= MAX_FILE_SIZE, {
+      message: "Max image size is 25MB.",
+    })
+});
 
 
 export type FormFieldProps = {
     type: string;
     placeholder: string;
-    name: ValidFieldNames;
+    name: keyof FormData;
     register: UseFormRegister<FormData>;
     error: FieldError | undefined;
     valueAsNumber?: boolean;
